60b70a0bd353811d9e532f1902fbb85051c46070
ahgameel <ahgameel@gmail.com>
2015-11-02 21:41:05 +0200

    Create New Project AngularJS With Asp.net WebAPI

---
+++ HomeCinema.Web/Scripts/spa/layout/sideBar.directive.js
@@ -0,0 +1,6 @@
+﻿(function (app)
+{
+    'use strict';
+    app.directive('sideBar', sideBar);
+    function sideBar() { return { restrict: 'E', replace: true, templateUrl: '/scripts/spa/layout/sideBar.html' } }
+})(angular.module('common.ui'));
